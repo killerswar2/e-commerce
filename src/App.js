b03dfdb46c40fa2087a274542099e4687b568620
killerswar2 <mheleheinzdebalucos@gmail.com>
2021-03-20 16:31:46 +0800

    Homepage made into smaller compinents, Finish stying menu-item component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HomePage></HomePage>
+        <HomePage />
       </div>
     );
   }
