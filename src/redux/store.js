096fd32fcee19e625a5b637e3055f009d7d4c145
killerswar2 <mheleheinzdebalucos@gmail.com>
2021-03-23 17:16:43 +0800

    Add redux and implemented userReducer and userActions to head and app components

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
-
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
